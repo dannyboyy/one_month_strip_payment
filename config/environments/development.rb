65fef5b4e6c7c349d0bf5dd341c18afa124522dc
Danny Wu <wushuo1988@hotmail.com>
2015-02-25 22:46:16 -0500

    Adding emailing of a purchase receipt

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Send emails using letter opener in dev mode to open emails
   config.action_mailer.delivery_method = :letter_opener
